f8103a84bb2616483b2073a0fd99236d4966244e
hasanariful <arifulhasan90r@gmail.com>
2023-10-12 17:01:05 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,47 @@
+console.log('I am ARIFUL HASAN')
+
+let x = 99
+if(x==100) {
+    console.log("Century")
+}
+else{
+    console.log("Not Century")
+}
+
+
+let place = "Bus_stand"
+let buses = 10
+let journeybus = "Khulna"
+
+if (place == "Bus_stand") {
+    if (buses >= 10) {
+       if (journeybus == "Khulna") {
+        console.log("start journey")
+       }
+       else{
+        console.log("don't start journey")
+       }
+    }
+    else{
+        console.log("there are few bus")
+    }
+}
+else{
+    console.log("no bus stand here")
+}
+
+for(i=10; i<=20; i++){
+    console.log(i)
+}
+
+
+let number = 13
+
+console.log(" multiplication table of 13 ")
+
+for (i=1; i<=10; i++){
+    console.log( + number + " x " + i + " = " + number*i)
+}
+
+// 5 x 1 = 5 
+
