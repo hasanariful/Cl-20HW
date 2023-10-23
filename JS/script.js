aed13573e91cc3c7b930fa5d427f537a7c3b9404
hasanariful <arifulhasan90r@gmail.com>
2023-10-23 12:17:17 +0600

    first commit

--- JS/script.js
+++ JS/script.js
@@ -1,47 +1,75 @@
-console.log('I am ARIFUL HASAN')
+// Random Gradesheet
+let number = 30
 
-let x = 99
-if(x==100) {
-    console.log("Century")
+if (number >=80 && number <=100) {
+    console.log("A+");
 }
-else{
-    console.log("Not Century")
+else if(number >=70 && number <=79) {
+    console.log("A");
 }
-
-
-let place = "Bus_stand"
-let buses = 10
-let journeybus = "Khulna"
-
-if (place == "Bus_stand") {
-    if (buses >= 10) {
-       if (journeybus == "Khulna") {
-        console.log("start journey")
-       }
-       else{
-        console.log("don't start journey")
-       }
-    }
-    else{
-        console.log("there are few bus")
-    }
+else if(number >=60 && number <=69) {
+    console.log("A-");
 }
-else{
-    console.log("no bus stand here")
+else if(number >=50 && number <=59) {
+    console.log("B");
 }
-
-for(i=10; i<=20; i++){
-    console.log(i)
+else if(number >=40 && number <=49) {
+    console.log("C");
 }
-
-
-let number = 13
-
-console.log(" multiplication table of 13 ")
-
-for (i=1; i<=10; i++){
-    console.log( + number + " x " + i + " = " + number*i)
+else if(number >=33 && number <=39) {
+    console.log("D");
 }
+else {
+    console.log("F");
+}
+// Random Gradesheet
 
-// 5 x 1 = 5 
+//Array with variables
+let fruits = ['mango', 'jackfruit', 'apple', 'watermelon']
+console.log( fruits)
 
+let members = [
+    {
+        name: 'Kamal',
+        age: 18,
+        hometown: 'Rangpur',
+    },
+
+    {
+        name: 'Rakib',
+        age: 20,
+        hometown: 'Comilla',
+    },
+]
+console.log(members)
+
+//Array with variables
+
+
+// Function
+function abc(){
+    let x = 10
+    let y = 20
+    console.log(x+y)
+    console.log(y-x)
+    console.log(x*y)
+    console.log(x/y)
+}
+abc()
+
+let company = ["Pran", "RFL", "Jamuna", "Basundhara", "YKK", "BAT"]
+
+company.forEach(function(comp){ 
+    console.log(comp)
+})
+// Function
+
+let text = document.querySelector('h1')
+let button = document.querySelector('button')
+button.addEventListener('click', function(){
+    text.style.color="blue"
+    text.style.background="red"
+    text.style.fontSize="50px"
+})
+
+ console.log(text.innerHTML)
