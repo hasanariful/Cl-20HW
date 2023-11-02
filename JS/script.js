2f8560d304435bc31b2805f08dc4b061fcfb2c86
hasanariful <arifulhasan90r@gmail.com>
2023-11-02 16:39:27 +0600

    first commit

--- JS/script.js
+++ JS/script.js
@@ -1,75 +1,43 @@
-// Random Gradesheet
-let number = 30
-
-if (number >=80 && number <=100) {
-    console.log("A+");
-}
-else if(number >=70 && number <=79) {
-    console.log("A");
-}
-else if(number >=60 && number <=69) {
-    console.log("A-");
-}
-else if(number >=50 && number <=59) {
-    console.log("B");
-}
-else if(number >=40 && number <=49) {
-    console.log("C");
-}
-else if(number >=33 && number <=39) {
-    console.log("D");
-}
-else {
-    console.log("F");
-}
-// Random Gradesheet
-
-//Array with variables
-let fruits = ['mango', 'jackfruit', 'apple', 'watermelon']
-console.log( fruits)
-
-let members = [
-    {
-        name: 'Kamal',
-        age: 18,
-        hometown: 'Rangpur',
-    },
-
-    {
-        name: 'Rakib',
-        age: 20,
-        hometown: 'Comilla',
-    },
-]
-console.log(members)
-
-//Array with variables
-
-
-// Function
-function abc(){
-    let x = 10
-    let y = 20
-    console.log(x+y)
-    console.log(y-x)
-    console.log(x*y)
-    console.log(x/y)
-}
-abc()
-
-let company = ["Pran", "RFL", "Jamuna", "Basundhara", "YKK", "BAT"]
-
-company.forEach(function(comp){ 
-    console.log(comp)
-})
-// Function
-
-let text = document.querySelector('h1')
-let button = document.querySelector('button')
-button.addEventListener('click', function(){
-    text.style.color="blue"
-    text.style.background="red"
-    text.style.fontSize="50px"
-})
-
- console.log(text.innerHTML)
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    //I'm adding this section so I don't have to keep updating this pen every year :-)
+    //remove this if you don't need it
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 1,
+        dayMonth = "10/23/",
+        birthday = dayMonth + yyyy;
+    
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > birthday) {
+      birthday = dayMonth + nextYear;
+    }
+    //end
+    
+    const countDown = new Date(birthday).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          //do something later when date is reached
+          if (distance < 0) {
+            document.getElementById("headline").innerText = "It's my birthday!";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("content").style.display = "block";
+            clearInterval(x);
+          }
+          //seconds
+        }, 0)
+    }());
